efd6a9b9b5b7a56ca2b02c08315ba2dd510079f2
Alisson Youssf <youssfbr@gmail.com>
2023-10-11 18:44:51 -0300

    Project created

---
+++ src/test/java/com/github/youssfbr/patterndio/PatternDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.patterndio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
