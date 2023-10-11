e302b9972ca9a0c70dc9cfe0c94896cadfa9feae
Alisson Youssf <youssfbr@gmail.com>
2023-10-11 19:27:22 -0300

    ORM

--- src/main/java/com/github/youssfbr/patterndio/entities/Clients.java
+++ src/main/java/com/github/youssfbr/patterndio/entities/Clients.java
@@ -1,18 +1,30 @@
 package com.github.youssfbr.patterndio.entities;
 
 
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.Instant;
 import java.time.LocalDate;
 import java.util.List;
 
+@Data
+@Entity(name = "tb_client")
 public class Clients {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private LocalDate birthDate;
+
+    @ToString.Exclude
+    @OneToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE })
     private List<Phone> phones;
+
     @CreationTimestamp
     @Column(nullable = false,
             updatable = false,
--- src/main/java/com/github/youssfbr/patterndio/entities/Phone.java
+++ src/main/java/com/github/youssfbr/patterndio/entities/Phone.java
@@ -1,9 +1,21 @@
 package com.github.youssfbr.patterndio.entities;
 
 import com.github.youssfbr.patterndio.entities.enums.PhoneType;
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
+@Entity(name = "tb_phone")
 public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 10)
     private PhoneType phoneType;
+
+    @Column(nullable = false, length = 20)
     private String number;
 }
